39d075d8450d00a2ed2903d9275df404fdae015d
Elliot Sayes <elliot@sayes.cloud>
2023-02-01 16:39:40 +0800

    Use larger buffers for encryption stream I/O

--- lib/services/crypto/stream_cipher.dart
+++ lib/services/crypto/stream_cipher.dart
@@ -1,23 +1,61 @@
 import 'dart:async';
 import 'dart:typed_data';
 
+import 'package:async/async.dart';
 import 'package:pointycastle/export.dart';
 
-import '../../utils/stream.dart';
-
 const _aesBlockSizeBytes = 16;
-final _blockChunker = chunkTransformer(_aesBlockSizeBytes);
+const _bufferSizeBytes = 1024;
 
-Stream<Uint8List> _encryptBlockStream(GCMBlockCipher encrypter, Stream<Uint8List> blockStream) async * {
-  await for (final block in blockStream) {
-    final encryptedBlockBuffer = Uint8List(block.length);
-    /*final blockProcessedBytesCount = */encrypter.processBlock(block, 0, encryptedBlockBuffer, 0);
-    yield encryptedBlockBuffer/*.sublist(0, blockProcessedBytesCount)*/;
+Stream<Uint8List> _encryptBlockStream(GCMBlockCipher encrypter, Stream<Uint8List> dataStream) async* {
+  var sinkBuffer = Uint8List(_bufferSizeBytes);
+  var sinkBufferOffset = 0;
+
+  List<Uint8List> handleSink(Uint8List data, {bool flush = false}) {
+    final toYield = <Uint8List>[];
+
+    // yield & reset buffer if adding would result in an overflow
+    if (sinkBufferOffset + data.length > _bufferSizeBytes - 1) {
+      toYield.add(Uint8List.sublistView(sinkBuffer, 0, sinkBufferOffset));
+      sinkBuffer = Uint8List(_bufferSizeBytes);
+      sinkBufferOffset = 0;
+    }
+    
+    // add to sink buffer
+    sinkBuffer.setAll(sinkBufferOffset, data);
+    sinkBufferOffset += data.length;
+
+    // yield remaining when flushing
+    if (flush) {
+      toYield.add(Uint8List.sublistView(sinkBuffer, 0, sinkBufferOffset));
+    }
+
+    return toYield;
   }
+
+  final chunker = ChunkedStreamReader(dataStream);
+  while (true) {
+    final block = await chunker.readBytes(_aesBlockSizeBytes);
+    if (block.isEmpty) break;
+
+    final encryptedBlockBuffer = Uint8List(block.length);
+    final blockProcessedBytesCount = encrypter.processBlock(block, 0, encryptedBlockBuffer, 0);
+    
+    final toYield = handleSink(Uint8List.sublistView(encryptedBlockBuffer, 0, blockProcessedBytesCount));
+    for (final chunk in toYield) {
+      yield chunk;
+    }
+  }
+
   final macBuffer = Uint8List(encrypter.macSize);
   final macBytesCount = encrypter.doFinal(macBuffer, 0);
+  
   if (macBytesCount != macBuffer.length) throw StateError('MAC size mismatch: $macBytesCount != ${macBuffer.length}');
-  yield macBuffer;
+  
+  final toYield = handleSink(macBuffer, flush: true);
+  for (final chunk in toYield) {
+    yield chunk;
+  }
 }
 
 /// Encrypts the given data stream with AES-256GCM using the given key and initialization vector (IV).
@@ -32,12 +70,8 @@ StreamTransformer<Uint8List, Uint8List> aes256GcmEncryptionTransformer(Uint8List
   final encrypter = GCMBlockCipher(AESEngine())
     ..init(true, parameters);
   
-  final blockEncrypter = StreamTransformer.fromBind(
+  return StreamTransformer.fromBind(
     (Stream<Uint8List> blockStream) => _encryptBlockStream(encrypter, blockStream),
-  );
-
-  return StreamTransformer.fromBind((stream) =>
-    stream.transform(_blockChunker).transform(blockEncrypter),
   );
 }
 
--- test/services/crypto/stream_cipher_test.dart
+++ test/services/crypto/stream_cipher_test.dart
@@ -10,11 +10,11 @@ final _testKey = Uint8List.fromList(List.generate(256 ~/ 8, (index) => index % 2
 final _testIV = Uint8List.fromList(List.generate(12, (index) => index % 256));
 
 void main() {
-  group('encryptAes256GcmStream function', () {
-    final encryptionTransformer = aes256GcmEncryptionTransformer(_testKey, _testIV);  
-    
+  group('encryptAes256GcmStream function', () {    
     test('should match sync method when a multiple of block size', () async {
-      dataGenerator() => generateKilobytesOfData(1, finalSizeBytes: _blockSize);
+      final encryptionTransformer = aes256GcmEncryptionTransformer(_testKey, _testIV);
+      
+      dataGenerator() => generateKilobytesOfData(1, finalSizeBytes: _blockSize * 3);
       final dataStream = dataGenerator();
       final dataBuffer = await collectBytes(dataGenerator());
       expect(dataBuffer.length % _blockSize, equals(0));
@@ -24,10 +24,13 @@ void main() {
 
       final bufferEncryptedData = await bufferEncrypt(dataBuffer, _testKey, _testIV);
       
+      expect(streamEncryptedData.length, equals(bufferEncryptedData.length));
       expect(streamEncryptedData, equals(bufferEncryptedData));
     });
 
     test('should match sync method when not a multiple of block size', () async {
+      final encryptionTransformer = aes256GcmEncryptionTransformer(_testKey, _testIV);
+      
       dataGenerator() => generateKilobytesOfData(1, finalSizeBytes: _blockSize + 3);
       final dataStream = dataGenerator();
       final dataBuffer = await collectBytes(dataGenerator());
@@ -38,10 +41,13 @@ void main() {
 
       final bufferEncryptedData = await bufferEncrypt(dataBuffer, _testKey, _testIV);
       
+      expect(streamEncryptedData.length, equals(bufferEncryptedData.length));
       expect(streamEncryptedData, equals(bufferEncryptedData));
     });
 
     test('should match sync method when zero length', () async {
+      final encryptionTransformer = aes256GcmEncryptionTransformer(_testKey, _testIV);  
+      
       dataGenerator() => generateKilobytesOfData(0);
       final dataStream = dataGenerator();
       final dataBuffer = await collectBytes(dataGenerator());
@@ -52,13 +58,21 @@ void main() {
 
       final bufferEncryptedData = await bufferEncrypt(dataBuffer, _testKey, _testIV);
       
+      expect(streamEncryptedData.length, equals(bufferEncryptedData.length));
       expect(streamEncryptedData, equals(bufferEncryptedData));
     });
 
     test('should encrypt a lot of data', () async {
-      final dataStream = generateKilobytesOfData(1000, finalSizeBytes: 255);
+      final encryptionTransformer = aes256GcmEncryptionTransformer(_testKey, _testIV);
+      
+      final dataStream = generateKilobytesOfData(200, finalSizeBytes: 255);
       final encryptionStream = dataStream.transform(encryptionTransformer);
-      await for (final _ in encryptionStream) {}
+      
+      await for (final _ in encryptionStream) {
+        print(_.length);
+      }
+      
+      expect(true, isTrue);
     }, timeout: const Timeout(Duration(minutes: 1)));
   });
 }
